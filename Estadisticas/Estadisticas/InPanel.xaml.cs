53c780844b2ebfd96f34db0a17f0671be13db63a
AlexMiranda7 <47649414+AlexMiranda7@users.noreply.github.com>
2020-08-23 17:54:01 -0600

    Big Upload

---
+++ Estadisticas/Estadisticas/InPanel.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Estadisticas
+{
+    /// <summary>
+    /// Interaction logic for InPanel.xaml
+    /// </summary>
+    public partial class InPanel : Window
+    {
+        public InPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnoff_Click(object sender, RoutedEventArgs e)
+        {
+           
+            MessageBoxResult cierre = MessageBox.Show("¿Esta seguro de salir?", "Cierre de aplicación", MessageBoxButton.YesNo);
+            if (cierre == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+               
+            }
+            
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void lstmenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = lstmenu.SelectedIndex;
+            MoveEtiquetaMenu(index);
+
+            switch (index)
+            {
+                case 0:
+                    GridInPanel.Children.Clear();
+                    GridInPanel.Children.Add(new USCInicio());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveEtiquetaMenu(int index)
+        {
+            etiqueta.OnApplyTemplate();
+            GridEtiqueta.Margin = new Thickness(0,(75 +( 42* index)),0,(290-(42 * index)));
+        }
+
+    }
+}
---
+++ Estadisticas/Estadisticas/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Estadisticas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[,] login = { { "rmiranda", "rvillalta" }, {"$3nt!n3la","ingreso20" } };
+        int intentos = 0;
+        public MainWindow()
+        {
+            System.Threading.Thread.Sleep(300);
+            InitializeComponent();
+        }
+
+        private void Button_ColorChanged(object sender, RoutedPropertyChangedEventArgs<Color> e)
+        {
+
+        }
+
+        private void btningresar_Click(object sender, RoutedEventArgs e)
+        {
+            int j;
+            for (j = 0; j < 4; j++)
+            {
+                if (txtusuario.Text != login[0, j] && pst.Password != login[1, j])
+                {
+                    j++;
+                }
+                if (txtusuario.Text == login[0, j] && pst.Password == login[1, j])
+                {
+                    this.Hide();
+                    InPanel i = new InPanel();
+                    i.Show(); break;
+                }
+                if (intentos == 3)
+                {
+                    MessageBox.Show("Has alcanzado el numero maximo de intentos, cerrando sistema", "Mensaje del sistema", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    intentos++;
+                    MessageBox.Show("Error de autenticacion, verifique usuario y/O contraseña ó es posible que su cuenta este inhabilitada", "Mensaje del sistema", MessageBoxButton.OK, MessageBoxImage.Information);
+                    txtusuario.Clear();
+                    pst.Clear();
+                    break;
+                }
+
+            }            
+          
+        }
+    }
+}
